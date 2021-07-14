b705795dd6f7b38475d43f73d8f49600ad658500
Martin Stewart <MartinSStewart@gmail.com>
2021-07-14 17:58:02 +0200

    Work on docs

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # elm-review-remove-duplicate-code
 
-Provides [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rules to REPLACEME.
-
-
-## Provided rules
-
-- [`RemoveDuplicateCode`](https://package.elm-lang.org/packages/MartinSStewart/elm-review-remove-duplicate-code/1.0.0/RemoveDuplicateCode) - Reports REPLACEME.
+This [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rule detects when some code has been copied too many times and should instead be refactored into a single function.
+A heuristic is used to estimate when some code has been copied too many times. A long block of code that has been copied 3 times might get reported while a very short piece of code might not get reported even when copied hundreds of times.
+Additionally, this rule is more lenient on code in test modules since often you want to duplicate expected values in order to easily understand and edit tests.
 
 
 ## Configuration
@@ -18,9 +15,14 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ RemoveDuplicateCode.rule
+    [ RemoveDuplicateCode.rule { ignore = [], threshold = 25 }
     ]
 ```
+
+* `ignore` tells this rule to skip some modules when counting code duplication (though it will still mention all the places it found the code, even if some instances are in ignored modules)
+* `threshold` determines the total "complexity" the duplicate code can have before it gets reported.
+This value doesn't represent any easily explained metric but 25 is a good starting point and if you find that it reports code you don't want to de-duplicate, you can increase it.
+To help with determining a good threshold, each error which will tell you how much complexity it had.
 
 
 ## Try it out
--- src/RemoveDuplicateCode.elm
+++ src/RemoveDuplicateCode.elm
@@ -22,19 +22,36 @@ import Review.ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Review.Rule as Rule exposing (Error, ModuleRuleSchema, Rule)
 
 
-{-| This rule reports when it finds code that has been copied too many times and should instead be made into a reusable function.
+{-| This [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rule detects when some code has been copied too many times and should instead be refactored into a single function.
+A heuristic is used to estimate when some code has been copied too many times.
+A long block of code copied 3 times might get reported while a very short piece of code might not get reported even when copied hundreds of times.
+Additionally, this rule is more lenient on code in test modules since often you want to duplicate expected values in order to easily understand and edit tests.
 
-Note that "too many times" depends both on the number of copies and how much code is being copied.
-You can write `Element.width Element.fill` hundreds of times and you won't get an error.
-But if you were to copy paste a hundred line block of code twice then this rule would tell you to place it in a function instead.
+
+## Configuration
+
+    module ReviewConfig exposing (config)
+
+    import RemoveDuplicateCode
+    import Review.Rule exposing (Rule)
+
+    config : List Rule
+    config =
+        [ RemoveDuplicateCode.rule { ignore = [], threshold = 25 }
+        ]
+
+  - `ignore` tells this rule to skip some modules when counting code duplication (though it will still mention all the places it found the code, even if some instances are in ignored modules)
+  - `threshold` determines the total "complexity" the duplicate code can have before it gets reported.
+    This value doesn't represent any easily explained metric but 25 is a good starting point and if you find that it reports code you don't want to de-duplicate, you can increase it.
+    To help with determining a good threshold, each error which will tell you how much complexity it had.
 
 
 ## Try it out
 
-You can try this rule out by running the following command:
+You can try the example configuration above out by running the following command:
 
 ```bash
-elm-review --template MartinSStewart/elm-review-remove-duplicate-code/example --rules RemoveDuplicateCode
+elm-review --template MartinSStewart/elm-review-remove-duplicate-code/example
 ```
 
 -}
@@ -62,6 +79,7 @@ type ModuleIgnoreType
     | IncludeSubmodules
 
 
+{-| -}
 type alias Config =
     { ignore : List ( ModuleIgnoreType, List String ), threshold : Int }
 
